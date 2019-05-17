d47f21c5e81cde07e6c7b978e8a5b41a0f391b37
hedong <hedong@ibeesaas.com>
2019-05-17 20:16:58 +0800

    视频添加

--- src/main/java/com/oushuai/animal/controller/VideoController.java
+++ src/main/java/com/oushuai/animal/controller/VideoController.java
@@ -1,23 +1,104 @@
 package com.oushuai.animal.controller;
 
-import com.oushuai.animal.bean.Video;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.oushuai.animal.bean.*;
+import com.oushuai.animal.service.AlbumService;
 import com.oushuai.animal.service.CategoryService;
+import com.oushuai.animal.service.NewService;
 import com.oushuai.animal.service.VideoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @Controller
+@RequestMapping("/video")
 public class VideoController {
     @Autowired
     private VideoService videoService;
-@GetMapping("/video")
-String list(Model model){
-    List<Video> video = videoService.list();
-    model.addAttribute("list",video);
-    return "video/play";
-}
+    @Autowired
+    private AlbumService albumService;
+
+    @RequestMapping(value = "/list/{albumId}", method = RequestMethod.GET)
+    public String list(@PathVariable("albumId") Integer albumId, @RequestParam(value = "pn", defaultValue = "1") Integer pn, Model model) {
+        PageHelper.startPage(pn, 5);
+        List<Video> video = videoService.list(albumId);
+        Album album = albumService.getInfo(albumId);
+        model.addAttribute("list", video);
+        PageInfo page = new PageInfo(video, 5);
+        model.addAttribute("albumName", album.getAlbumName());
+        model.addAttribute("pageInfo", page);
+        return "videos/list";
+    }
+
+    @RequestMapping("/delete")
+    @ResponseBody
+    public Msg delete(Integer videoId) {
+        boolean del = videoService.delete(videoId);
+        return del ? Msg.success() : Msg.fail();
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String showAdd() {
+        return "video/add";
+    }
+
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public String upload(Video video, MultipartFile file, HttpServletRequest reqeust) {
+        Msg uploadResult = uploadVideo(video, file, reqeust);
+        if (uploadResult.getCode() == 0) {
+            video.setVideoDate(new Date());
+            videoService.insert(video);
+            return "video/list";
+        } else {
+            return "video/add";
+        }
+    }
+    public Msg uploadVideo(Video video,MultipartFile  file, HttpServletRequest reqeust)  {
+        InputStream stream=null;
+        try {
+            stream =  file.getInputStream();
+        } catch (IOException e) {
+            return Msg.fail();
+        }
+        String fileName=file.getName();
+        fileName= UUID.randomUUID().toString()+"_"+fileName;
+        String dest= "E:/animalsite/animal/src/main/web/WEB-INF/video/";
+        String s=dest+fileName+".jpg";
+        video.setVideoUrl("http://localhost:8080/images/"+fileName+".jpg");
+        FileOutputStream fis= null;
+        try {
+            fis = new FileOutputStream(new File(s));
+            byte[] buffer=new byte[1024];
+            int len= 0;
+            while( (len=stream.read(buffer))!=-1){
+                fis.write(buffer,0,len);
+            }
+            Msg msg=new Msg();
+            msg.setMsg(fileName);
+            return msg;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Msg.fail();
+        }finally {
+            if(fis!=null){
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
--- src/main/java/com/oushuai/animal/service/VideoService.java
+++ src/main/java/com/oushuai/animal/service/VideoService.java
@@ -1,17 +1,43 @@
 package com.oushuai.animal.service;
 
 import com.oushuai.animal.bean.Photo;
+import com.oushuai.animal.bean.PhotoExample;
 import com.oushuai.animal.bean.Video;
+import com.oushuai.animal.bean.VideoExample;
 import com.oushuai.animal.dao.PhotoMapper;
 import com.oushuai.animal.dao.VideoMapper;
 import org.springframework.stereotype.Service;
+
 import javax.annotation.Resource;
 import java.util.List;
+
 @Service
 public class VideoService {
     @Resource
     VideoMapper videoMapper;
-    public List<Video> list(){
-        return  videoMapper.selectByExample(null);
+
+    public List<Video> list() {
+        return videoMapper.selectByExample(null);
+    }
+
+
+    public List<Video> list(Integer albumId) {
+        VideoExample example = new VideoExample();
+        VideoExample.Criteria criteria = example.createCriteria();
+        criteria.andAlbumIdEqualTo(albumId);
+        //   PageHelper.startPage(pageIndex,pageSize, true);
+        List<Video> list = videoMapper.selectByExample(example);
+        return list;
+
+    }
+
+    public void insert(Video video) {
+        videoMapper.insert(video);
+    }
+
+
+    public boolean delete(Integer videoId) {
+        int result = videoMapper.deleteByPrimaryKey(videoId);
+        return result <= 0 ? true : false;
     }
 }
